bfd3bf6024612b5bd357896243ffc47c73dca739
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-09-05 13:31:41 +0200

    use ivy-ssl context instead of setting up custom trust
    - set it up as property; instead of globally overriding the SslContext
    for JVM

--- mailstore-connector/src/com/axonivy/connector/mailstore/MailSessionProvider.java
+++ mailstore-connector/src/com/axonivy/connector/mailstore/MailSessionProvider.java
@@ -6,68 +6,94 @@ import java.util.Properties;
 
 import javax.mail.Session;
 
-import com.axonivy.connector.oauth.ssl.SSLContextConfigure;
+import org.apache.commons.lang3.BooleanUtils;
+
+import com.axonivy.connector.mailstore.enums.StartTLS;
 
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.ssl.restricted.IvySslSocketFactory;
+import ch.ivyteam.ivy.ssl.restricted.SslClientSettings;
+import ch.ivyteam.ivy.ssl.restricted.SslConfig;
 import ch.ivyteam.ivy.vars.Variable;
 
 public class MailSessionProvider {
 
-  private static final String PROPERTIES_VAR = "properties";
+	private static final String PROPERTIES_VAR = "properties";
 
-  static Session getSession(String storeName) throws Exception {
-    Properties properties = getProperties(storeName);
-    if (SSLContextConfigure.get().isStartTLSEnabled(properties)) {
-      SSLContextConfigure.get().addIvyTrustStoreToCurrentContext();
-    }
+	private interface Property {
+		String MAIL_SMTP_SSL_SOCKET_FACTORY = "mail.smtp.ssl.socketFactory";
+		String MAIL_SMTP_SSL_SOCKET_FACTORY_FALLBACK = "mail.smtp.ssl.socketFactory.fallback";
+	}
 
-    return Session.getInstance(properties, null);
-  }
+	static Session getSession(String storeName) throws Exception {
+		Properties properties = getProperties(storeName);
+		if (isStartTLSEnabled(properties)) {
+			properties.put(Property.MAIL_SMTP_SSL_SOCKET_FACTORY, ivySslContext());
+			// ensures that socket will only be created with our socket factory otherwise it
+			// will fail
+			properties.setProperty(Property.MAIL_SMTP_SSL_SOCKET_FACTORY_FALLBACK, "false");
+		}
 
-  static Session getSession() {
-    Properties properties = getProperties();
-    return Session.getDefaultInstance(properties, null);
-  }
+		return Session.getInstance(properties, null);
+	}
 
-  static Properties getProperties() {
-    Properties properties = System.getProperties();
+	private static boolean isStartTLSEnabled(Properties properties) {
+		if (properties == null) {
+			return false;
+		}
+		return BooleanUtils.toBoolean(properties.getProperty(StartTLS.ENABLE.getProperty()))
+				|| BooleanUtils.toBoolean(properties.getProperty(StartTLS.REQUIRED.getProperty()));
+	}
 
-    String propertiesPrefix = PROPERTIES_VAR + ".";
-    for (Variable variable : Ivy.var().all()) {
-      String name = variable.name();
-      if (name.startsWith(propertiesPrefix)) {
-        String propertyName = name.substring(propertiesPrefix.length());
-        String value = variable.value();
-        LOG.info("Setting additional property {0}: ''{1}''", propertyName, value);
-        properties.setProperty(name, value);
-      }
-    }
+	private static IvySslSocketFactory ivySslContext() {
+		return new IvySslSocketFactory(new SslConfig(false, "noAlias", SslClientSettings.instance()));
+	}
 
-    return properties;
-  }
+	static Session getSession() {
+		Properties properties = getProperties();
+		return Session.getDefaultInstance(properties, null);
+	}
 
-  // Only retrieve properties from the store that belong to
-  static Properties getProperties(String storeName) {
-    Properties properties = new Properties();
-    String propertiesPrefix = String.format("%s.%s.%s.", MailStoreService.MAIL_STORE_VAR, storeName, PROPERTIES_VAR);
+	static Properties getProperties() {
+		Properties properties = System.getProperties();
 
-    for (Variable variable : Ivy.var().all()) {
-      String name = variable.name();
-      if (name.contains(propertiesPrefix)) {
-        String propertyName = getSubstringAfterProperties(name);
-        String value = variable.value();
-        LOG.info("Setting additional property {0}: ''{1}''", propertyName, value);
-        properties.setProperty(propertyName, value);
-      }
-    }
+		String propertiesPrefix = PROPERTIES_VAR + ".";
+		for (Variable variable : Ivy.var().all()) {
+			String name = variable.name();
+			if (name.startsWith(propertiesPrefix)) {
+				String propertyName = name.substring(propertiesPrefix.length());
+				String value = variable.value();
+				LOG.info("Setting additional property {0}: ''{1}''", propertyName, value);
+				properties.setProperty(name, value);
+			}
+		}
 
-    return properties;
-  }
+		return properties;
+	}
 
-  private static String getSubstringAfterProperties(String input) {
-    int index = input.indexOf(PROPERTIES_VAR);
+	// Only retrieve properties from the store that belong to
+	static Properties getProperties(String storeName) {
+		Properties properties = new Properties();
+		String propertiesPrefix = String.format("%s.%s.%s.", MailStoreService.MAIL_STORE_VAR, storeName,
+				PROPERTIES_VAR);
 
-    return index != -1 ? input.substring(index + PROPERTIES_VAR.length() + 1) : null;
-  }
+		for (Variable variable : Ivy.var().all()) {
+			String name = variable.name();
+			if (name.contains(propertiesPrefix)) {
+				String propertyName = getSubstringAfterProperties(name);
+				String value = variable.value();
+				LOG.info("Setting additional property {0}: ''{1}''", propertyName, value);
+				properties.setProperty(propertyName, value);
+			}
+		}
+
+		return properties;
+	}
+
+	private static String getSubstringAfterProperties(String input) {
+		int index = input.indexOf(PROPERTIES_VAR);
+
+		return index != -1 ? input.substring(index + PROPERTIES_VAR.length() + 1) : null;
+	}
 
 }
